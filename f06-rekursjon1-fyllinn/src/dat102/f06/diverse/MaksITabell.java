9e0456a8f2de2d015c48038eafbda62258dadf5e
Andreas Skogly <andreas.skogly1@gmail.com>
2026-01-29 08:54:54 +0100

    e

--- f06-rekursjon1-fyllinn/src/dat102/f06/diverse/MaksITabell.java
+++ f06-rekursjon1-fyllinn/src/dat102/f06/diverse/MaksITabell.java
@@ -1,13 +1,28 @@
 package dat102.f06.diverse;
 
+import java.util.Arrays;
+
 public class MaksITabell {
 	
 	public static void main(String[] args) {
 		
 		int[] tabell = {-4, 5, 12, 0, 3, 7, -2, 3};
 		
-//		System.out.println("Den største verdien i tabellen " 
-//				+ Arrays.toString(tabell) + " er " + maksTab(tabell));
+		System.out.println("Den største verdien i tabellen " + Arrays.toString(tabell) + " er " + maksTab(tabell));
+	}
+
+	public static int maksTab (int[] t) {
+		return maksTab(t, 0, t.length-1);
+	}
+
+	public static int maksTab (int[] t, int start, int slutt) {
+		if (start == slutt) {
+			return t[start];
+		} else {
+			int midten = (start + slutt) / 2;
+			int maksV =
+		}
+
 	}
 
 }
